9cb3a8562003e74cf5fceca2800bff1c5ff13d0b
ElcioLS <elciols@gmail.com>
2022-11-02 22:09:29 -0300

    Alterando configurações http do GetConnect decoder

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -3,10 +3,27 @@ import 'package:get/get.dart';
 import '../models/user_model.dart';
 
 class UserRepository {
-  final restClient = GetConnect();
+  final restClient = GetConnect(timeout: const Duration(milliseconds: 600));
+
+  UserRepository() {
+    restClient.httpClient.baseUrl = 'http://192.168.0.5:8080';
+    // restClient.httpClient.errorSafety = false;
+  }
+
+  // Future<List<UserModel>> findAll() async {
+  //   // // O DECODER SEMPRE SERÁ CHAMADO MESMO SE SUA RESPOSTA TIVER BODY OU NÃO ********
+  //   // // NÃO É RECOMENDADO UTILIZAR!!! RAHMAN *****************************************
+  //   final result = await restClient.get(
+  //     '/users', decoder: (data) {
+  //       return data.map<UserModel>((user) => UserModel.fromMap(user)).toList();
+  //     },);
+  //   if (result.hasError) {throw Exception('Erro ao buscar usuário (${result.statusText})');}
+  //   return result.body ?? [];
+  // }
+  // // //
 
   Future<List<UserModel>> findAll() async {
-    final result = await restClient.get('http://192.168.0.5:8080/users');
+    final result = await restClient.get('/users');
 
     if (result.hasError) {
       throw Exception('Erro ao buscar usuário (${result.statusText})');
@@ -18,8 +35,7 @@ class UserRepository {
   }
 
   Future<void> save(UserModel user) async {
-    final result =
-        await restClient.post('http://192.168.0.5:8080/users', user.toMap());
+    final result = await restClient.post('/users', user.toMap());
 
     if (result.hasError) {
       throw Exception('Erro ao salvar usuário (${result.statusText})');
@@ -27,8 +43,7 @@ class UserRepository {
   }
 
   Future<void> deleteUser(UserModel user) async {
-    final result =
-        await restClient.delete('http://192.168.0.5:8080/users/${user.id}');
+    final result = await restClient.delete('/users/${user.id}');
 
     if (result.hasError) {
       throw Exception('Erro ao deletar usuário (${result.statusText})');
@@ -36,8 +51,7 @@ class UserRepository {
   }
 
   Future<void> updateUser(UserModel user) async {
-    final result = await restClient.put(
-        'http://192.168.0.5:8080/users/${user.id}', user.toMap());
+    final result = await restClient.put('/users/${user.id}', user.toMap());
 
     if (result.hasError) {
       throw Exception('Erro ao atualizar usuário (${result.statusText})');
